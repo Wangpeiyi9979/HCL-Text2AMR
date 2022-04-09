61bf45b87b1cc817e0e4b1feb4cf7866392d9a79
Peiyi Wang <42565075+Wangpeiyi9979@users.noreply.github.com>
2022-04-09 14:33:48 +0800

    Update evaluation.sh

--- amr-evaluation/evaluation.sh
+++ amr-evaluation/evaluation.sh
@@ -1,5 +1,8 @@
+#!/bin/bash
 
-out=`python smatch/smatch.py --pr -f "$1" "$2"`
+# Evaluation script. Run as: ./evaluation.sh <parsed_data> <gold_data>
+
+out=`smatch.py  --significant 4 --pr -f "$1" "$2"`
 pr=`echo $out | cut -d' ' -f2`
 rc=`echo $out | cut -d' ' -f4`
 fs=`echo $out | cut -d' ' -f6`
@@ -7,7 +10,7 @@ echo 'Smatch -> P: '$pr', R: '$rc', F: '$fs
 
 sed 's/:[a-zA-Z0-9-]*/:label/g' "$1" > 1.tmp
 sed 's/:[a-zA-Z0-9-]*/:label/g' "$2" > 2.tmp
-out=`python smatch/smatch.py --pr -f 1.tmp 2.tmp`
+out=`smatch.py --significant 4 --pr -f 1.tmp 2.tmp`
 pr=`echo $out | cut -d' ' -f2`
 rc=`echo $out | cut -d' ' -f4`
 fs=`echo $out | cut -d' ' -f6`
@@ -15,7 +18,7 @@ echo 'Unlabeled -> P: '$pr', R: '$rc', F: '$fs
 
 cat "$1" | perl -ne 's/(\/ [a-zA-Z0-9\-][a-zA-Z0-9\-]*)-[0-9][0-9]*/\1-01/g; print;' > 1.tmp
 cat "$2" | perl -ne 's/(\/ [a-zA-Z0-9\-][a-zA-Z0-9\-]*)-[0-9][0-9]*/\1-01/g; print;' > 2.tmp
-out=`python smatch/smatch.py --pr -f 1.tmp 2.tmp`
+out=`smatch.py --significant 4 --pr -f 1.tmp 2.tmp`
 pr=`echo $out | cut -d' ' -f2`
 rc=`echo $out | cut -d' ' -f4`
 fs=`echo $out | cut -d' ' -f6`
